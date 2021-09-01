d4c7dc652080a81303bf8bfaec21bf01d80b89a8
bevilaquabruno <bbbevilaqua@gmail.com>
2021-09-01 12:55:43 -0300

    ++ sql & connection

--- db/database.sql
+++ db/database.sql
@@ -8,9 +8,11 @@ CREATE TABLE country (
     name VARCHAR(50) NOT NULL,
     fullName VARCHAR(100),
     short CHAR(3) NOT NULL,
-    flag VARCHAR(50)
+    flag VARCHAR(50
 );
 
 /* Primary Keys */
-ALTER TABLE country ADD CONSTRAINT pk_country PRIMARY KEY (id)
+ALTER TABLE country CHANGE id id INT AUTO_INCREMENT PRIMARY KEY NOT NULL;
+
 /* Foreign Keys */
+
---
+++ db/db.ts
@@ -0,0 +1,11 @@
+import mysql from 'mysql';
+import * as dotenv from 'dotenv';
+
+var connection: mysql.Connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+export default connection;
