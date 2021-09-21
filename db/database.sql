ce9d8a2f9eeaeff540fdfa7b3a9feaac0f68bf1e
bevilaquabruno <bbbevilaqua@gmail.com>
2021-09-20 22:41:30 -0300

    ++ database

--- db/database.sql
+++ db/database.sql
@@ -13,14 +13,35 @@ CREATE TABLE IF NOT EXISTS country (
 
 CREATE TABLE IF NOT EXISTS user (
 	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  name varchar(50) NOT NULL,
   username varchar(20) NOT NULL UNIQUE,
-  email varchar(50) NOT NULL,
-  password varchar(100)
+  password varchar(100),
+  person_id INT NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS person (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(100) NOT NULL,
+  phone VARCHAR(20),
+  email VARCHAR(30) NOT NULL,
+  birth_date DATETIME,
+  death_date DATETIME,
+  spouse VARCHAR(100),
+  pseudonym_id INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS pseudonym (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  description VARCHAR(50)
+);
+
+/* Foreign Keys */
+ALTER TABLE user ADD CONSTRAINT fk_user_person_id FOREIGN KEY (person_id) REFERENCES person(id);
+
+ALTER TABLE person ADD CONSTRAINT fk_person_pseudonym_id FOREIGN KEY (pseudonym_id) REFERENCES pseudonym(id);
 
 /* PRIMARY INSERTS*/
 INSERT INTO country(name, fullname, short, flag, id) VALUES ("Brasil", "República Federativa do Brasil", "BRA", "brasil_flag.png", 1);
 
-INSERT INTO user(name, username, email, password) VALUES ("Bruno Fernando Bevilaqua", "bevilaqua", "bbbevilaqua@gmail.com", MD5("123"));
+INSERT INTO person (name, phone, email, birth_date) VALUES ("Bruno Fernando Bevilaqua", "5549998320023", 2000-03-05);
+
+INSERT INTO user(id_pessoa, username, password) VALUES (1, "bevilaqua", MD5("123"));
