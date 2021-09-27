2af153e75147627a9b80ed9e1402dd210877fd34
bevilaquabruno <bbbevilaqua@gmail.com>
2021-09-27 19:30:08 -0300

    + database using pool

--- db/db.ts
+++ db/db.ts
@@ -2,14 +2,19 @@ import mysql from "mysql2/promise";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+var pool: mysql.Pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
 class DatabaseConnection {
-  static async getConnection(): Promise<mysql.Connection> {
-    return await mysql.createConnection({
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-    });
+  static async getConnection(): Promise<mysql.Pool> {
+    return pool;
   }
 }
 
